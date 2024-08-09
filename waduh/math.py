3b45dc2d49cdb2166de672ed9fc83fd56acde918
indra87g <twinightwheel50@gmail.com>
2024-08-09 20:54:45 +0700

    style(math): code format

--- waduh/math.py
+++ waduh/math.py
@@ -1,9 +1,10 @@
 import math
 
+
 class Arithmetic:
     def __init__(self, method=str):
         self.set_method(method)
-        
+
     def set_method(self, method):
         if method == "add":
             self.method = self.add
@@ -15,21 +16,20 @@ class Arithmetic:
             self.method = self.distribute
         else:
             raise ValueError("Invalid arithmetic method.")
-        
+
     def add(self, x, y):
         return x + y
-    
+
     def multiply(self, x, y):
         return x * y
-    
+
     def substract(self, x, y):
         return x - y
-    
+
     def distribute(self, x, y):
         if y == 0:
             raise ValueError("Distribute with zero is not allowed.")
         return x / y
-    
+
     def calculate(self, x, y):
         return self.method(x, y)
-        
